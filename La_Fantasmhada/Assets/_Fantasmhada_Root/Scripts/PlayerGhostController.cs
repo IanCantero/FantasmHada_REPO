20c154f2126c0edd1784582850f7af9c1c38bb74
IanCantero <iancant94@gmail.com>
2026-01-09 09:54:55 +0100

    PlayerGhostCanMove

--- La_Fantasmhada/Assets/_Fantasmhada_Root/Scripts/PlayerGhostController.cs
+++ La_Fantasmhada/Assets/_Fantasmhada_Root/Scripts/PlayerGhostController.cs
@@ -7,24 +7,23 @@ public class PlayerGhostController : MonoBehaviour
     [Header("Movement and Jump Configuration")]
     [SerializeField] float speed;
     [SerializeField] float jumpForce;
-    [SerializeField] bool isGrounded;
+    //[SerializeField] bool isGrounded;
     [SerializeField] bool isFacingRight; //Define la orientacion del personaje
-    [SerializeField] Transform groundCheck; //Posición del detector del suelo
-    [SerializeField] float groundCheckRadius; //Define el radio del circulo de suelo
+    //[SerializeField] Transform groundCheck; //Posición del detector del suelo
+    //[SerializeField] float groundCheckRadius; //Define el radio del circulo de suelo
     [SerializeField] LayerMask groundLayer; //Define la capa que puede tocar el detector de suelo
 
-    [Header("Shoot Configuration")]
+    [Header("Shoot/Attack Configuration")]
     [SerializeField] GameObject projectile;  //Ref prefab bala
     [SerializeField] Transform shootPoint;  //Ref posicion donde dispara
-    [SerializeField] float shootCooldown = 1; //Tiempo de cooldown entre disparos
-    bool canShoot; 
+    bool canAttack; //Bool de seguridad que define si se puede atacar o no
 
     //Variables de referencia general
     Rigidbody2D playerRb; //Almacén del rigidbody del player
     Animator anim; //Almacén del controlador de animaciones del player
     PlayerInput input; //Almacén del controlador de inputs del player
     Vector2 moveInput; //Almacén del valor de los botones de movimiento
-    bool canAttack; //Bool de seguridad que define si se puede atacar o no
+   
 
     private void Awake()
     {
@@ -32,7 +31,6 @@ public class PlayerGhostController : MonoBehaviour
          anim = GetComponent<Animator>();
          input = GetComponent<PlayerInput>();
         canAttack = true;
-        canShoot = true;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -46,7 +44,7 @@ public class PlayerGhostController : MonoBehaviour
     void Update()
     {
         //Lógica de deteccion de suelo
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        //isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         //Lógica del flip del personaje
         if (moveInput.x > 0 && !isFacingRight) Flip();
         if (moveInput.x < 0 && isFacingRight) Flip();
@@ -68,36 +66,24 @@ public class PlayerGhostController : MonoBehaviour
         Vector3 currentScale = transform.localScale; //Almacén temporal de la escala del objeto
         currentScale.x *= -1; //Invertir el valor de X
         transform.localScale = currentScale; //Le devolvemos la escala al objeto con el valor x inverso
-        isFacingRight = !isFacingRight; //Decirle al bool que cambie el valor al contrarioz
+        isFacingRight = !isFacingRight; //Decirle al bool que cambie el valor al contrario
     }
     void Jump()
     {
         playerRb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
     }
 
-    void Shoot()
-    {
-        canShoot=false;
-        GameObject actualProjectile = Instantiate(projectile, shootPoint.position, Quaternion.identity);
-       Projectile projectileScript = actualProjectile.GetComponent<Projectile>();
-        projectileScript.isFacingRight = isFacingRight;  //Igualar la orientación de la bala a la del player
-        Invoke(nameof(ResetShoot), shootCooldown); //Se espera tanto tiempo como shootCooldown y entonces resetea el disparo
-    }
-    void ResetShoot()
-    {
-        canShoot = true;
-    }
 
     IEnumerator Attack()
     {
         canAttack = false;
-        float actualSpeed = speed; //Guarda speed acueal para devolverla luego
-        speed = 0;
-        anim.SetTrigger("AC_Duaine_Attack");
-        yield return new WaitForSeconds(0.8f);
-        speed = actualSpeed;
+        anim.SetTrigger("Attack");
+        GameObject actualProjectile = Instantiate(projectile, shootPoint.position, Quaternion.identity);
+        Projectile projectileScript = actualProjectile.GetComponent<Projectile>();
+        projectileScript.isFacingRight = isFacingRight;
+        yield return new WaitForSeconds(1.5f);
         canAttack = true;
-        //Devolvemos speed y capacidad de ataque, fin de la rutina
+        Debug.Log("Puedes atacar");
         yield return null;
 
     }
@@ -105,9 +91,9 @@ public class PlayerGhostController : MonoBehaviour
     void AnimationManagement()
     {
         //Acción para gestionar los cambios de animación
-        anim.SetBool("Jump", !isGrounded);
-        if (moveInput.x != 0) anim.SetBool("AC_Duaine_Run", true);
-        else anim.SetBool("AC_Duaine_Run", false);
+        //anim.SetBool("Jump", !isGrounded);
+        if (moveInput.x != 0) anim.SetBool("Run", true);
+        else anim.SetBool("Run", false);
 
     }
 
@@ -119,18 +105,11 @@ public class PlayerGhostController : MonoBehaviour
     }
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.performed && isGrounded) Jump();
+        if (context.performed) Jump();
     }
     public void OnAttack(InputAction.CallbackContext context)
     {
-        if (context.performed && isGrounded && canAttack) StartCoroutine(Attack());
+        if (context.performed && canAttack) StartCoroutine(Attack());
     }
-    public void OnShoot(InputAction.CallbackContext context)
-    {
-        //Lógica del inicio del disparo
-        if (context.performed && canShoot) Shoot();
-    }
-
-
     #endregion  
 }
