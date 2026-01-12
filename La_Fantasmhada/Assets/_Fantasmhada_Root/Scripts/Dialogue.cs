a5dd445044335cb6bbaf970e23e9dc478f09981a
IanCantero <136346391+IanCantero@users.noreply.github.com>
2026-01-12 19:56:44 +0100

    SimpleDialogueScript

---
+++ La_Fantasmhada/Assets/_Fantasmhada_Root/Scripts/Dialogue.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    bool isPlayerOnRange;
+    bool didDialogueStart;
+    int lineIndex;
+    float typingSpeed = 0.05f;
+
+    [SerializeField] GameObject dialogueMark;
+    [SerializeField, TextArea(4, 6)] string[] dialogueLines;
+    [SerializeField] GameObject dialoguePanel;
+    [SerializeField] TMP_Text dialogueText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerOnRange && Input.GetKeyDown("Attack"))
+        {
+            StartDialogue();
+        }
+        else if (dialogueText.text == dialogueLines[lineIndex])
+        {
+            NextLine();
+        }
+        else 
+        {
+            StopAllCoroutines();
+            dialogueText.text = dialogueLines[lineIndex];
+        }
+    }
+
+    private void StartDialogue()
+    {
+        didDialogueStart = true;
+        dialoguePanel.SetActive(true);
+        dialogueMark.SetActive(false);
+        lineIndex = 0;
+        Time.timeScale = 0f; // Para todo el game
+        StartCoroutine(ShowLine());
+    }
+
+    void NextLine()
+    {
+        lineIndex++;
+        if (lineIndex < dialogueLines.Length)
+        {
+            StartCoroutine(ShowLine());
+        }
+        else
+        {
+            didDialogueStart = false;
+            dialoguePanel.SetActive(false);
+            dialogueMark.SetActive(true);
+            Time.timeScale = 1f; // lo restablece
+        }
+    }
+
+    IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+
+        foreach(char ch in dialogueLines[lineIndex])
+        {
+                        dialogueText.text += ch;
+            yield return new WaitForSecondsRealtime(typingSpeed);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerOnRange = true;
+            dialogueMark.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerOnRange = false;
+            dialogueMark.SetActive(false);
+        }
+    }
+}
