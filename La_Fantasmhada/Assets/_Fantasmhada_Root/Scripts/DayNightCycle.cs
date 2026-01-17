a0cd8e6bee666eabc9021698edccfaba9555bddb
IanCantero <136346391+IanCantero@users.noreply.github.com>
2026-01-17 18:41:07 +0100

    Día y noche2

--- La_Fantasmhada/Assets/_Fantasmhada_Root/Scripts/DayNightCycle.cs
+++ La_Fantasmhada/Assets/_Fantasmhada_Root/Scripts/DayNightCycle.cs
@@ -3,45 +3,70 @@ using UnityEngine.Rendering.Universal;
 
 public class DayNightCycle : MonoBehaviour
 {
-    public Light2D globalLight;
 
+    [Header("Day Night Cycle Settings")]
+
+    public Light2D globalLight;
 
     [SerializeField] float cycleDuration = 120f;
     [SerializeField] Color morningColor;
     [SerializeField] Color dayColor;
     [SerializeField] Color eveningColor;
     [SerializeField] Color nightColor;
+    private Color actualColor;
 
     float time;
+
+    [Header("Player Swap Settings")]
+
+    [SerializeField] GameObject dayPlayer;
+    [SerializeField] GameObject nightPlayer;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        dayPlayer.SetActive(true);
+        nightPlayer.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-     time += Time.deltaTime;
+        PlayerSwitch();
+        time += Time.deltaTime;
         float t = time  / cycleDuration;
 
         if (t < 0.25f) 
         {
             globalLight.color = Color.Lerp(morningColor, dayColor, t/0.25f);
+            actualColor = morningColor;
         }
         else if (t < 0.5f) 
         {
             globalLight.color = Color.Lerp(dayColor, eveningColor, (t - 0.25f) / 0.25f);
+            actualColor = dayColor;
         }
         else if (t < 0.75f) 
         {
             globalLight.color = Color.Lerp(eveningColor, nightColor, (t - 0.50f) / 0.25f);
+            actualColor = eveningColor;
         }
         else 
         {
             globalLight.color = Color.Lerp(nightColor, morningColor, (t - 0.75f) / 0.25f);
+            actualColor = nightColor;
+        }
+    }
+
+    void PlayerSwitch()
+    {
+        if (actualColor == nightColor)
+        {
+            nightPlayer.transform.position = dayPlayer.transform.position;
+            nightPlayer.transform.rotation = dayPlayer.transform.rotation;
+            dayPlayer.SetActive(false);
+            nightPlayer.SetActive(true);
         }
     }
 }
